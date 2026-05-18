6221023244a5d472adf54a9a47585acd36cdbcad
jvmotadev <jvmotadev67@gmail.com>
2026-05-18 20:16:21 -0300

    saldo disponivel + formatar saldo

--- app/src/main/java/com/example/cashswap/controller/ConvertActivity.kt
+++ app/src/main/java/com/example/cashswap/controller/ConvertActivity.kt
@@ -1,14 +1,19 @@
 package com.example.cashswap.controller
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.cashswap.R
 import com.example.cashswap.model.Moeda
+import java.text.NumberFormat
+import java.util.Locale
 
 class ConvertActivity : AppCompatActivity() {
 
@@ -32,7 +37,28 @@ class ConvertActivity : AppCompatActivity() {
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, nomes)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
-        findViewById<Spinner>(R.id.spinnerOrigem).adapter = adapter
+        val spinnerOrigem = findViewById<Spinner>(R.id.spinnerOrigem)
+        spinnerOrigem.adapter = adapter
         findViewById<Spinner>(R.id.spinnerDestino).adapter = adapter
+
+        val tvSaldoDisponivel = findViewById<TextView>(R.id.tvSaldoDisponivel)
+        spinnerOrigem.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                val moeda = moedas[position]
+                tvSaldoDisponivel.text = "Saldo disponível: ${formatarSaldo(moeda)}"
+            }
+            override fun onNothingSelected(parent: AdapterView<*>) {}
+        }
+    }
+
+    private fun formatarSaldo(moeda: Moeda): String {
+        val fmt = NumberFormat.getNumberInstance(Locale("pt", "BR")).apply {
+            minimumFractionDigits = moeda.casasDecimais
+            maximumFractionDigits = moeda.casasDecimais
+        }
+        return if (moeda.codigo == "BTC")
+            "${fmt.format(moeda.saldo)} ${moeda.simbolo}"
+        else
+            "${moeda.simbolo} ${fmt.format(moeda.saldo)}"
     }
 }
